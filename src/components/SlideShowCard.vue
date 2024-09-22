7ce18baf3f3201b659cf8d1c80ddc7dde37ce53e
Zeng Kun <1489782679@qq.com>
2024-09-22 15:31:37 +0800

    重构： - 采用 Vue3 框架 更新： - 新增主页轮播图 计划： - 无

---
+++ src/components/SlideShowCard.vue
@@ -0,0 +1,133 @@
+<script setup lang="ts">
+
+import { ref, watch } from 'vue';
+import card1 from '../assets/showcards/card1.png'
+import card2 from '../assets/showcards/card2.png'
+import card3 from '../assets/showcards/card3.png'
+
+const cardSize = { width: 20, height: 30 }
+const cardSpace = 1
+
+const showCardList = [
+  {
+    url: card1, saying: '死亡不属于工人阶级', author: '列宁'
+  },
+  {
+    url: card2, saying: '世界是属于你们的', author: '毛主席'
+  },
+  {
+    url: card3, saying: '不能再后悔了 全力以赴', author: '鲁迪乌斯'
+  }
+]
+
+const selectedIndex = ref(0)
+const transLeft = ref(0)
+
+watch(selectedIndex, (newIndex,_)=>{
+  transLeft.value = -(cardSize.width + cardSpace * 2) * newIndex
+})
+
+function onCardClicked(index: number) {
+  selectedIndex.value = index
+}
+
+
+// 默认选择中间的
+selectedIndex.value = Math.floor(showCardList.length / 2)
+
+// onSelectedIndexChanged(selectedIndex.value,selectedIndex.value)
+
+</script>
+
+<template>
+  <div class="slideshow-shell">
+    <div class="slideshow-container">
+      <div class="slideshow-image-container" v-for="(card, index) in showCardList">
+        <div @click="onCardClicked(index)" :class="[selectedIndex == index ? 'show-card selected' : 'show-card']">
+          <img :src="card.url" :alt="card.saying" />
+          <p class="show-card-saying">{{ card.saying }}</p>
+          <h4 class="show-card-author">—— {{ card.author }}</h4>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.slideshow-shell {
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+
+}
+
+.slideshow-container {
+  display: flex;
+  position: relative;
+
+  left: calc(50% - v-bind('(cardSize.width + cardSpace * 2) / 2 - transLeft + "rem"'));
+  width: fit-content;
+
+  transition: all .2s;
+}
+
+.slideshow-image-container {}
+
+.show-card {
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+
+  width: v-bind('cardSize.width + "rem"');
+  height: v-bind('cardSize.height + "rem"');
+  margin: 1rem v-bind('cardSpace + "rem"');
+
+  border-radius: 0.5rem;
+  border-style: solid;
+  box-sizing: content-box;
+  border-color: transparent;
+  border-width: 0.25rem;
+  background-color: var(--dark-control-color);
+
+  align-items: center;
+
+  box-shadow: 0.2rem 0.2rem 0.4rem 0.2rem rgb(from var(--dark-control-color) r g b / .33);
+
+  transition: all .2s;
+
+
+  &:hover,
+  &.selected {
+    border-color: #e67e22;
+    box-shadow: 0.2rem 0.2rem 0.6rem 0.3rem rgb(from #e67e22 r g b / .44);
+    transform: translateY(-0.25rem);
+  }
+
+  &>img {
+    display: inline-block;
+    /* transform: translateX(-30%); */
+    height: 75%;
+    min-width: 100%;
+    object-fit: cover;
+  }
+}
+
+.show-card-saying {
+
+  margin-top: 0.5rem;
+
+  font-family: '宋体', system-ui;
+  font-size: 1.5rem;
+  line-height: 2rem;
+  text-align: center;
+  width: 100%;
+}
+
+.show-card-author {
+  font-size: 2rem;
+
+  line-height: 4rem;
+  width: 100%;
+  text-align: center;
+}
+</style>
